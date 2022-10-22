03075fd582a51cdd33953d0b4c4c28bea666ab65
AK <masha.kulyk2006@gmail.com>
2022-10-22 09:58:37 +0300

    Init

---
+++ cw1.py
@@ -0,0 +1,68 @@
+print("1) 2+2=?")
+print("a) 3")
+print("b) 4")
+print("c) 9")
+
+count = 0
+answer1 = input("Choose correct answer: ")
+
+if answer1 == "b":
+    print("Correct!")
+    count+=1
+elif answer1 != "b":
+    print("Not correct")
+
+print("1) 4*4=?")
+print("a) 16")
+print("b) 4")
+print("c) 8")
+
+answer2 = input("Choose correct answer: ")
+
+if answer2 == "a":
+    print("Correct!")
+    count+=1
+elif answer1 != "a":
+    print("Not correct")
+
+print("1) 11^2=?")
+print("a) 22")
+print("b) 234")
+print("c) 121")
+
+answer3 = input("Choose correct answer: ")
+
+if answer3 == "c":
+    print("Correct!")
+    count+=1
+elif answer3 != "c":
+    print("Not correct")
+
+print("1) 66/3=?")
+print("a) 64")
+print("b) 22")
+print("c) 12")
+
+answer4 = input("Choose correct answer: ")
+
+if answer4 == "b":
+    print("Correct!")
+    count+=1
+elif answer4 != "b":
+    print("Not correct")
+
+print("1) 45/15=?")
+print("a) 5")
+print("b) 3")
+print("c) 2")
+
+answer5 = input("Choose correct answer: ")
+
+if answer5 == "b":
+    print("Correct!")
+    count+=1
+elif answer5 != "b":
+    print("Not correct")
+
+
+
